228a564213a7cfe10e7388d647a5e4634b0a1701
TTOJlTOPA <zhuk.valeriy.98@gmail.com>
2017-10-12 23:51:16 +0300

    Initial commit

---
+++ desktop/src/by/emptythoughts/colonizer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package by.emptythoughts.colonizer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import by.emptythoughts.colonizer.ColonizerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ColonizerGame(), config);
+	}
+}
